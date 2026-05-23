c291dff653d5b5a3f7806495d6fda2b0e7189b5c
WuYihui <qq2568520309@gmail.com>
2026-05-23 17:01:45 +0800

    添加了基本要素

---
+++ RoboticArm/Source/RoboticArm/Public/MainController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTICARM_API AMainController : public APlayerController
+{
+	GENERATED_BODY()
+};
